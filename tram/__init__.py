aff46a0c8326fa6555be590b8ff6e03e0f53f870
aminn <aminn@med.uni-greifswald.de>
2026-07-20 11:21:11 +0200

    locationname to locationid

--- tram/__init__.py
+++ tram/__init__.py
@@ -64,7 +64,7 @@ class Sample(Idable):
     receiptdate:datetime=None
     first_repositiondate:datetime=None
     initialamount:Amount=None
-    locationname:str=None
+    locationid:str=None
     locationpath:str=None
     orga:str = None
     parent:Idable=None 
@@ -98,7 +98,7 @@ class Sample(Idable):
          initialamount:Amount=None,
          kitid:str=None,
          locationpath:str=None,
-         locationname:str=None,
+         locationid:str=None,
          orga:str=None,
          parent:Idable=None,
          patient:Idable=None,
@@ -133,7 +133,7 @@ class Sample(Idable):
         self.initialamount = initialamount
         self.kitid = kitid
         self.locationpath = locationpath
-        self.locationname = locationname
+        self.locationid = locationid
         self.orga = orga
         self.parent = parent
         self.patient = patient
